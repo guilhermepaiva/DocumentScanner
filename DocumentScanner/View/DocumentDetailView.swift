02434ad7999de6287a801bc2c14bea97b043a2b3
Guilherme Paiva <guilhermepaiva9@gmail.com>
2025-01-17 09:08:07 -0300

    Adds detail view and delete and share features

--- DocumentScanner/View/DocumentDetailView.swift
+++ DocumentScanner/View/DocumentDetailView.swift
@@ -6,8 +6,16 @@
 //
 
 import SwiftUI
+import PDFKit
 
 struct DocumentDetailView: View {
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) private var context
+    
+    @State private var isLoading: Bool = false
+    @State private var showFileMover: Bool = false
+    @State private var fileURL: URL?
+    
     var document: Document
     var body: some View {
         if let pages = document.pages?.sorted(by: { $0.pageIndex < $1.pageIndex }) {
@@ -34,6 +42,13 @@ struct DocumentDetailView: View {
             }
             .background(.black)
             .toolbarVisibility(.hidden, for: .navigationBar)
+            .loadingScreen(status: $isLoading)
+            .fileMover(isPresented: $showFileMover, file: fileURL) { result in
+                // removing the temporary file
+                guard let fileURL else { return }
+                try? FileManager.default.removeItem(at: fileURL)
+                self.fileURL = nil
+            }
         }
     }
     
@@ -45,9 +60,7 @@ struct DocumentDetailView: View {
             .hSpacing(.center)
             .overlay(alignment: .trailing) {
                 // lock button
-                Button {
-                    
-                } label: {
+                Button(action: createAndShareDocument) {
                     Image(systemName: document.isLocked ? "lock.fill" : "lock.open.fill")
                         .font(.title3)
                         .foregroundStyle(.purple)
@@ -70,7 +83,12 @@ struct DocumentDetailView: View {
             Spacer(minLength: 8)
             
             Button {
-                //
+                dismiss()
+                Task { @MainActor in
+                    try? await Task.sleep(for: .seconds(0.3))
+                    context.delete(document)
+                    try? context.save()
+                }
             } label: {
                 Image(systemName: "trash.fill")
                     .font(.title3)
@@ -78,7 +96,37 @@ struct DocumentDetailView: View {
             }
         }
         .padding([.horizontal, .bottom], 15)
+    }
+    
+    private func createAndShareDocument() {
+        // converting a swiftdata document into a pdf document
+        guard let pages = document.pages?.sorted(by: { $0.pageIndex < $1.pageIndex }) else { return }
+        isLoading = true
         
+        Task.detached(priority: .high) { [document] in
+            try? await Task.sleep(for: .seconds(0.2))
+            
+            let pdfDocument = PDFDocument()
+            for index in pages.indices {
+                if let pageImage = UIImage(data: pages[index].pageData),
+                   let pdfPage = PDFPage(image: pageImage) {
+                    pdfDocument.insert(pdfPage, at: index)
+                }
+            }
+            
+            var pdfURL = FileManager.default.temporaryDirectory
+            let fileName = "\(document.name).pdf"
+            pdfURL.append(path: fileName)
+            
+            if pdfDocument.write(to: pdfURL) {
+                await MainActor.run { [pdfURL] in
+                    fileURL = pdfURL
+                    showFileMover = true
+                    isLoading = false
+                }
+                
+            }
+        }
     }
 }
 
